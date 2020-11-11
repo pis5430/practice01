03bd75ac0cc80b1b0b8568ce0015dfd277b7eaa9
pyo i seul <isl1089@naver.com>
2020-11-12 00:20:00 +0900

    practice Scanner부분 수정중 06

---
+++ src/com/javaex/practice/EX20.java
@@ -0,0 +1,42 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class EX20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("500원 개수: ");
+		int a = sc.nextInt();
+		
+		System.out.print("100원 개수: ");
+		int b = sc.nextInt();
+		
+		System.out.print("50원 개수: ");
+		int c = sc.nextInt();
+		
+		System.out.print("10원 개수: ");
+		int d = sc.nextInt();
+		
+		
+		int a500 = a*500;
+		int b100 = b*100;
+		int c50 = c*50;
+		int d10 = d*10;
+		
+		int all = a500+b100+c50+d10;
+		
+		
+		System.out.println("동전의 총합은" + all + " 원 입니다.");
+		
+		
+		
+		sc.close();
+
+	}
+
+}
--- src/com/javaex/practice/Ex19.java
+++ src/com/javaex/practice/Ex19.java
@@ -3,7 +3,23 @@ package com.javaex.practice;
 public class Ex19 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		// 거리 = 속도 * 시간
+		// 1년 = 360*24*60*60
+		// km = 300000 * (365*24*60*60) 
+		
+		long light = 300000L;
+		long year = 365*24*60*60;
+
+		long km = light*year;
+		
+		
+		System.out.println("빛이 1년동안 가는 거리는 " + km + " 입니다.");
+		
+		
+		
+		
 
 	}
 
